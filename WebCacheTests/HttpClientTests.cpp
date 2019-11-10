4928c699834b9367099879a034bffa50da0434a7
Panda <sylvain.chevremont@gmail.com>
2019-11-10 10:20:46 +0100

    typo

--- WebCacheTests/HttpClientTests.cpp
+++ WebCacheTests/HttpClientTests.cpp
@@ -10,7 +10,7 @@ TEST(HttpClientTests, get_a_file)
   EXPECT_EQ(9169, ret.message.size());
 }
 
-TEST(HttpClientTests, fail_a_connection) //this test requires to on a machine with no http server on 666
+TEST(HttpClientTests, fail_a_connection) //this test requires to run on a machine with no http server on 666
 {
   BasicHttpClient c;
   ASSERT_THROW(c.get("http://localhost:666"), HttpClientException);
